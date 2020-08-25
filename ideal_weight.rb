af38dd32dc0e27b1178b657e17d8f9445d250016
wenzel <wenzel@debian>
2020-08-25 19:56:45 +0500

    My first program on github

---
+++ ideal_weight.rb
@@ -0,0 +1,19 @@
+print "Привет, давай знакомится! "
+user_name = gets.capitalize.chomp
+
+print "Введите пожалуйста свой рост! "
+user_height = gets.chomp.to_i
+
+ideal_user_weight = (user_height - 110) * 1.15
+
+
+print "Уважаемый #{user_name} ваш идеальный вес: #{ideal_user_weight}"
+
+puts
+
+if ideal_user_weight < 0
+	puts "Ваш вес уже оптимальный"
+end
+
+
+
